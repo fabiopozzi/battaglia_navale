66e8f7aef264a0b88c74ccf0ea3891437b532b2d
Fabio Pozzi <pozzi.fabio@gmail.com>
2020-11-19 14:15:27 +0100

    Disegna tabellone di gioco vuoto.

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 #include<ncurses.h>
+
+#define MSG_INIZIALE "Battaglia navale v 0.0.1"
 
 int righe, colonne;
 
@@ -13,8 +16,24 @@ void create_player(void)
 
 void riempi_tabellone(void)
 {
+    int r, c;
+    char lettera[] = "0";
+    const int dim_tab = 10;
+    const int off_iniz_righe = 5;
+    const int off_iniz_col = 5;
 
-    printf("disegna tabellone");
+    clear(); // pulisci schermo
+    /* intestazione colonne */
+    for (c = 0; c < dim_tab; c++){
+        mvprintw(off_iniz_righe - 1, off_iniz_col + (2 * c), lettera);
+        lettera[0]++;
+    }
+    /* intestazione righe */
+    lettera[0] = 'a';
+    for (r = 0; r < dim_tab; r++){
+        mvprintw(off_iniz_righe + r, off_iniz_righe - 1, lettera);
+        lettera[0]++;
+    }
 }
 
 void init_screen(void)
@@ -23,15 +42,17 @@ void init_screen(void)
     getmaxyx(stdscr, righe, colonne); /* ottieni la dimensione del terminale */
     keypad(stdscr, TRUE);			/* permetti uso F1-F12 e tasti freccia */
     noecho();						/* no keyboard echo per l'input utente */
-    mvprintw(righe/2, (colonne/2) - 15, "Battaglia navale v.0.0.1");
+    int col_messaggio = (colonne/2) - (strlen(MSG_INIZIALE) / 2);
+    mvprintw(righe/2, col_messaggio, MSG_INIZIALE);
     refresh();
 }
 
 int main(int argc, char *argv[])
 {
     init_screen();
+    getch();
     //create_player();
-    //riempi_tabellone();
+    riempi_tabellone();
     //game_loop();
 
     getch();
